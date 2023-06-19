8ead3e97c7b26d5336a8d39de8c7567f58d911a9
seiuna <nmmlwc@gmail.com>
2023-06-20 02:25:04 +0800

    first

--- src/main/java/cn/seiua/Main.java
+++ src/main/java/cn/seiua/Main.java
@@ -9,9 +9,9 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        AfdApi afdApi=AfdApi.build("userid","token");
-        Optional<AfdOrder> afdOrder=afdApi.queryOrder("123"); //订单号
-        Optional<AfdOrders> afdOrders=afdApi.queryOrder(1); //按页查订单
-        Optional<AfdSponsers> afdSponsers=afdApi.querySponsor(1); //按页查赞助者
+//        AfdApi afdApi=AfdApi.build("userid","token");
+//        Optional<AfdOrder> afdOrder=afdApi.queryOrder("123"); //订单号
+//        Optional<AfdOrders> afdOrders=afdApi.queryOrder(1); //按页查订单
+//        Optional<AfdSponsers> afdSponsers=afdApi.querySponsor(1); //按页查赞助者
     }
 }
